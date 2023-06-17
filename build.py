c93c0fc40ced1fa127cad03914429710a72b3663
Ellie Leela Ang <git@elia.garden>
2023-06-17 20:49:59 +0200

    Remove priv repos

--- build.py
+++ build.py
@@ -11,6 +11,7 @@ import subprocess
 import sass
 import time
 
+
 # Load input data & jinja
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates'))
 with open('data.yml') as f:
@@ -24,6 +25,7 @@ for ignore in data["ignored_files"]:
     cmd += ["--exclude-dir", ignore]
 
 # Get repositories
+data['git'] = []
 with urllib.request.urlopen('https://git.elia.garden/api/v1/repos/search?uid=1') as url:
     repos = json.loads(url.read().decode())['data']
     for repo in repos:
